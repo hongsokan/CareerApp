cce2b31b4a3db3e53907eb58153050232070e3f6
hongsokan_mac <hongsokan@daum.net>
2020-07-02 18:30:51 +0900

    0702 imageview & picker

--- Career/Career/MypageViewController.swift
+++ Career/Career/MypageViewController.swift
@@ -10,35 +10,52 @@ import UIKit
 
 class MypageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    lazy var imagePicker: UIImagePickerController = {
-        let picker: UIImagePickerController = UIImagePickerController()
-        picker.sourceType = .photoLibrary
-        picker.delegate = self
-        return picker
-    }()
-    
     var imageView: UIImageView = {
         let image = UIImageView()
-        image.backgroundColor = .black
+        image.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        image.backgroundColor = .gray
         return image
     }()
     
-    func touchUpSelectImageButton(_ sender: UIButton) {
-        self.present(self.imagePicker, animated: true, completion: nil)
-    }
-
+    lazy var picker: UIImagePickerController = {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary // 방식 선택. 앨범에서 가져오는걸로 선택.
+        picker.allowsEditing = false // 수정가능하게 할지 선택. 하지만 false
+        picker.delegate = self // picker delegate
+        return picker
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         self.view.backgroundColor = .white
         
-        self.setConstraint()
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(pickImage))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        
+        self.setViews()
     }
     
+    func setViews() {
+        self.view.addSubview(self.imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalTo(view)
+            make.trailing.equalTo(view)
+            make.height.equalTo(self.imageView.snp.width)
+        }
+    }
+    
+    @objc func pickImage() {
+        self.present(self.picker, animated: true, completion: nil)
+    }
     
     // MARK: imagePicker 델리데이트
-    
     // 취소하면 모달 dismiss
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.dismiss(animated: true, completion: nil)
@@ -46,7 +63,7 @@ class MypageViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     // 선택하면
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
- 
+        
         if let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             self.imageView.image = originalImage
         }
@@ -55,28 +72,14 @@ class MypageViewController: UIViewController, UIImagePickerControllerDelegate, U
         
     }
     
-    func setConstraint() {
-        self.view.addSubview(imageView)
-
-        // imageView 레이아웃
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
-            make.leading.equalTo(view)
-            make.trailing.equalTo(view)
-            make.height.equalTo(imageView.snp.width)
-        }
-    }
-    
-
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
