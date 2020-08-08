8d226af52837f94d9c49a8da43b923eadfbfa2a4
hongsokan_mac <hongsokan@daum.net>
2020-08-08 11:15:38 +0900

    0808 login popup added

---
+++ Career/Career/LoginPopupViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginPopupViewController.swift
+//  Career
+//
+//  Created by 안홍석 on 2020/08/08.
+//  Copyright © 2020 안홍석. All rights reserved.
+//
+
+import UIKit
+
+class LoginPopupViewController: UIViewController {
+
+    // UIView
+    // Email, Password TextField
+    // SignIn, Cancel
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
